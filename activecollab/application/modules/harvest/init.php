35071b7f58f56537bf5fa2f905d5b0242b2cfabb
Andreas Schempp <andreas@schempp.ch>
2011-03-28 13:21:51 +0000

    harvest: Try to solve problem with ProjectConfigOptions not available on uninstallation

--- activecollab/application/modules/harvest/init.php
+++ activecollab/application/modules/harvest/init.php
@@ -8,6 +8,7 @@ define('HARVEST_MODULE', 'harvest');
 define('HARVEST_MODULE_PATH', APPLICATION_PATH . '/modules/harvest');
 
 require_once HARVEST_MODULE_PATH . '/functions.php';
+require_once SYSTEM_MODULE_PATH . '/models/ProjectConfigOptions.class.php';
 
 set_for_autoload(array
 (
