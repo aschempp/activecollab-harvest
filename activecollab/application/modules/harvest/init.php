aebb543b6a857d252ce7d955bc74dbe4c8bcc944
Andreas Schempp <andreas@schempp.ch>
2012-01-07 13:36:48 +0100

    Removed include of functions.php

--- activecollab/application/modules/harvest/init.php
+++ activecollab/application/modules/harvest/init.php
@@ -7,7 +7,6 @@
 define('HARVEST_MODULE', 'harvest');
 define('HARVEST_MODULE_PATH', APPLICATION_PATH . '/modules/harvest');
 
-require_once HARVEST_MODULE_PATH . '/functions.php';
 require_once ANGIE_PATH .'/classes/harvest/init.php';
 require_once SYSTEM_MODULE_PATH . '/models/ProjectConfigOptions.class.php';
 
